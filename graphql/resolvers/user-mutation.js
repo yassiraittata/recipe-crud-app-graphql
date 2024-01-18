875e6e9eb277a72387f3d83b519c12b838cb4f73
Yassir aittata <yassir.aittata@gmail.com>
2024-01-18 22:01:16 +0000

    add signup

--- graphql/resolvers/user-mutation.js
+++ graphql/resolvers/user-mutation.js
@@ -1,6 +1,34 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const User = require("../../models/user");
+
 const mutation = {
-  signup(parent, { id }, context) {
-    
+  async signup(parent, { username, email }, context) {
+    const userExist = await User.find({ username });
+    if (userExist) throw new Error("User already exists");
+
+    const hash = await bcrypt.hash(password, 12);
+
+    const user = new User({
+      username,
+      password: hash,
+    });
+
+    const createdUser = await user.save();
+
+    const token = jwt.sign(
+      { username, userId: createdUser.id },
+      process.env.SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    return {
+      token,
+      user,
+    };
   },
 };
 
